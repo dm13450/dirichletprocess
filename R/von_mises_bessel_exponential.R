ec60814fae6f3875cf30e278635f77fc7d0c7590
Kees Mulder <keestimmulder@gmail.com>
2018-07-01 03:20:16 +0200

    Several tries for prior draws

--- R/von_mises_bessel_exponential.R
+++ R/von_mises_bessel_exponential.R
@@ -1,8 +1,9 @@
-#' Create a von Mises mixing distribution
+#'Create a von Mises mixing distribution
 #'
-#' See \code{\link{DirichletProcessGaussian}} for details on the base measure.
 #'
-#'@param priorParameters Prior parameters for the base measure.
+#'@param priorParameters Prior parameters for the base measure which are, in
+#'  order, (mu_0, R_0, c).
+#'
 #'@return Mixing distribution object
 #'@export
 vonMisesMixtureCreate <- function(priorParameters){
@@ -15,22 +16,75 @@ logBesselI <- function(x, nu) log(besselI(x, nu, expon.scaled = TRUE)) + x
 # von Mises
 dvm <- function(x, mu, kp) {
   logpdf <- kp * cos(x - mu) - log(2*pi) - logBesselI(kp, 0)
-  exp(logpdf)
+  return(exp(logpdf))
 }
+
+library(flexcircmix)
+library(circglmbayes)
 
 Likelihood.vonmises <- function(mdobj, x, theta) dvm(x, theta[[1]], theta[[2]])
 
 PriorDraw.vonmises <- function(mdobj, n = 1) {
 
   priorParameters <- mdobj$priorParameters
+  mu_0 <- priorParameters[1]
+  R_0  <- priorParameters[2]
+  c    <- priorParameters[3]
 
-  lambda <- rgamma(n, priorParameters[3], priorParameters[4])
-  mu <- rnorm(n, priorParameters[1], (priorParameters[2] * lambda)^(-0.5))
-  theta <- list(array(mu, dim = c(1, 1, n)), array(sqrt(1/lambda), dim = c(1, 1,
-                                                                           n)))
+  if (c < 0 | R_0 < -1) stop("Prior parameters out of bounds.")
+
+  kp <- rbesselexp(n, c, R_0)
+  mu <- vapply(kp, function(kpi) {
+    rvmc(1, mu_0, -R_0 * kpi / c)
+    }, FUN.VALUE = 0)
+
+  theta <- list(array(mu, dim = c(1, 1, n)),
+                array(kp, dim = c(1, 1, n)))
   return(theta)
 }
 
+
+PriorDraw.vonmises <- function(mdobj, n = 1, nsamp = 5) {
+
+  priorParameters <- mdobj$priorParameters
+  mu_0 <- priorParameters[1]
+  R_0  <- priorParameters[2]
+  c    <- priorParameters[3]
+
+  if (c < 0 | R_0 < -1) stop("Prior parameters out of bounds.")
+
+  kp <- rbesselexp(n, c, R_0)
+  mu <- vapply(kp, function(kpi) {
+    rvmc(1, mu_0, -R_0 * kpi / c)
+  }, FUN.VALUE = 0)
+
+  theta <- list(array(mu, dim = c(1, 1, n)),
+                array(kp, dim = c(1, 1, n)))
+  return(theta)
+}
+
+
+
+dbesselexp2 <- function(kp, mu, mu_n, R_n, n_n) {
+  eta <- n_n
+  g <- -R_n * cos(mu - mu_n) / n_n
+  dbesselexp(kp, eta, g)
+}
+rbesselexp2 <- function(kp, mu, mu_n, R_n, n_n) {
+  eta <- n_n
+  g <- -R_n * cos(mu - mu_n) / n_n
+  rbesselexp(kp, eta, g)
+}
+
+
+# // Sample a new value for kappa.
+etag    = - R_psi * cos(b0_cur - psi_bar);
+sk_res  = sampleKappa(etag, n_post);
+kp_cur  = sk_res(0);
+
+dbesselexp(3, 15, 9)
+draws <- PriorDraw.vonmises(list(priorParameters = c(mu_0 = 0, g = -19/20, c = 20)), n = 10000)
+plot(cbind(mu = draws[[1]], kp = draws[[2]]))
 # PosteriorParameters.normal <- function(mdobj, x) {
 #
 #   priorParameters <- mdobj$priorParameters
