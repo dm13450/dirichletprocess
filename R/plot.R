99d15e5a2871879f07f20d5865923e3d276a5280
Kees Mulder <keestimmulder@gmail.com>
2018-09-21 17:52:26 +0200

    Remove unused param

--- R/plot.R
+++ R/plot.R
@@ -8,7 +8,6 @@
 #' @param x Dirichlet Process Object to plot
 #' @param likelihood Logical, indicating whether to plot the likelihood from the dpobj.
 #' @param single Logical, indicating whether to draw the posterior from the last iteration or use the full cluster sequence.
-#' @param y Consistency for generic.
 #' @param ... Graphical arguments
 #' @return A ggplot object
 #' @export
