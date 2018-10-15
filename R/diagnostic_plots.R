5e103e08c80e867e28568ece4544bf40b028af1f
Kees Mulder <keestimmulder@gmail.com>
2018-10-15 17:49:44 +0200

    Fix diagnostic plots

--- R/diagnostic_plots.R
+++ R/diagnostic_plots.R
@@ -18,12 +18,12 @@ DiagnosticPlots <- function(dpobj, gg = FALSE) {
   oldpar <- par()
   par(mfrow = c(2, 2))
 
-  if ("alphaChain"  %in% names(dpobj)) AlphaTraceplot(dpobj, gg =  )
-  if ("alphaChain"  %in% names(dpobj)) AlphaPriorPosteriorPlot(dpobj, gg = FALSE)
-  if ("labelsChain" %in% names(dpobj)) ClusterTraceplot(dpobj, gg = FALSE)
-  if ("likelihoodChain" %in% names(dpobj)) LikelihoodTraceplot(dpobj, gg = FALSE)
+  if ("alphaChain"  %in% names(dpobj)) AlphaTraceplot(dpobj, gg = gg)
+  if ("alphaChain"  %in% names(dpobj)) AlphaPriorPosteriorPlot(dpobj, gg = gg)
+  if ("labelsChain" %in% names(dpobj)) ClusterTraceplot(dpobj, gg = gg)
+  if ("likelihoodChain" %in% names(dpobj)) LikelihoodTraceplot(dpobj, gg = gg)
 
-  suppressWarnings(par(oldpar))
+  suppressWarnings(par(mfrow = oldpar$mfrow))
 
 }
 
@@ -34,11 +34,12 @@ DiagnosticPlots <- function(dpobj, gg = FALSE) {
 AlphaTraceplot <- function(dpobj, gg = TRUE) {
 
   if (gg) {
-    ggplot2::ggplot(data.frame(Alpha = dpobj$alphaChain,
+    p <- ggplot2::ggplot(data.frame(Alpha = dpobj$alphaChain,
                                Index = seq_along(dpobj$alphaChain)),
                     ggplot2::aes_string("Index", "Alpha")) +
       ggplot2::geom_line() +
       ggplot2::ggtitle("Traceplot of alpha")
+    return(p)
   } else {
     plot(dpobj$alphaChain, type = "l", ylab = "Alpha",
          main = "Traceplot of alpha")
@@ -57,17 +58,17 @@ AlphaPriorPosteriorPlot <- function(dpobj, prior_color = "#2c7fb8", post_color =
 
   if (gg) {
 
-    ggplot2::ggplot() +
+    p <- ggplot2::ggplot() +
       ggplot2::geom_histogram(data = data.frame(Alpha = dpobj$alphaChain),
                               mapping = ggplot2::aes_string("Alpha",
                                                             "..density..",
                                                             colour = "'Posterior'", fill = "'Posterior'"),
-                              bins = min(its / 10, 100),
-                              ) +
+                              bins = min(its / 10, 100)) +
       ggplot2::stat_function(fun = prior_fun,
                              mapping = ggplot2::aes_string(colour = "'Prior'")) +
       ggplot2::ggtitle("Prior and posterior of alpha") +
       ggplot2::scale_colour_manual(labels = c("Posterior", "Prior"), values = c(prior_color, post_color), aesthetics = c("colour", "fill"), name = " ")
+    return(p)
   } else {
     hist(dpobj$alphaChain, freq = FALSE, breaks = min(its / 10, 100),
          xlab = "Alpha", main = "Prior and posterior of alpha")
@@ -84,12 +85,13 @@ ClusterTraceplot <- function(dpobj, gg = TRUE) {
   n_clust <- sapply(dpobj$labelsChain, function(x) length(unique(x)))
 
   if (gg) {
-    ggplot2::ggplot(data.frame(nclust = n_clust,
+    p <- ggplot2::ggplot(data.frame(nclust = n_clust,
                                Index = seq_along(n_clust)),
                     ggplot2::aes_string("Index", "nclust")) +
       ggplot2::geom_line() +
       ggplot2::ylab("Number of clusters") +
       ggplot2::ggtitle("Traceplot of the number of clusters")
+    return(p)
   } else {
     plot(n_clust, type = "l", ylab = "Number of clusters",
          main = "Traceplot of the number of clusters")
@@ -103,12 +105,13 @@ ClusterTraceplot <- function(dpobj, gg = TRUE) {
 #'   each iteration.
 LikelihoodTraceplot <- function(dpobj, gg = TRUE) {
   if (gg) {
-    ggplot2::ggplot(data.frame(Lik = dpobj$likelihoodChain,
+    p <- ggplot2::ggplot(data.frame(Lik = dpobj$likelihoodChain,
                                Index = seq_along(dpobj$likelihoodChain)),
                     ggplot2::aes_string("Index", "Lik")) +
       ggplot2::geom_line() +
       ggplot2::ylab("Log-likelihood") +
       ggplot2::ggtitle("Traceplot of the log-likelihood")
+    return(p)
   } else {
     plot(dpobj$likelihoodChain, type = "l", ylab = "Log-likelihood",
          main = "Traceplot of the log-likelihood")
