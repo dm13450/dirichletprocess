803e7ff5bdf58c5dfbe38aa72b38297f7a5b3704
dm13450 <dean.markwick@talk21.com>
2019-04-09 15:25:46 +0100

    Cleaned up Alpha update function

--- R/update_concentration.R
+++ R/update_concentration.R
@@ -7,15 +7,19 @@ update_concentration <- function(oldParam, n, nParams, priorParameters){
   pi2 <- n * (priorParameters[2] - log(x))
   pi1 <- pi1/(pi1 + pi2)
 
-  if (runif(1) < pi1) {
-    g1 <- rgamma(1, priorParameters[1] + nParams, priorParameters[2] - log(x))
-    new_alpha <- g1
-  } else {
-    g2 <- rgamma(1, priorParameters[1] + nParams - 1, priorParameters[2] - log(x))
-    new_alpha <- g2
-  }
+  postParams1 <- priorParameters[1] + nParams
+  postParams2 <- priorParameters[2] - log(x)
 
-  #new_alpha
+  if (runif(1) > pi1) {
+    #g1 <- rgamma(1, priorParameters[1] + nParams, priorParameters[2] - log(x))
+    #new_alpha <- g1
+    postParams1 <- postParams1 - 1
+  } #else {
+    #g2 <- rgamma(1, priorParameters[1] + nParams - 1, priorParameters[2] - log(x))
+    #new_alpha <- g2
+  #}
+
+  new_alpha <- rgamma(1, postParams1, postParams2)
   return(new_alpha)
 }
 
