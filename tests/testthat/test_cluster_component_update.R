bcd99a6e3a9a1df8f8ec1c4304cfbd2c4d46e0a2
giovanni-sig <giovanni.sighinolfi2@studio.unibo.it>
2020-03-20 17:57:08 +0100

    Added test for Mv Normal

--- tests/testthat/test_cluster_component_update.R
+++ tests/testthat/test_cluster_component_update.R
@@ -1,13 +1,26 @@
 context("Cluster Component Update")
 
 
-test_that("Hierarchical 5 Data", {
+test_that("Hierarchical Beta 5 Data", {
 
   dataTest <- list(rbeta(10, 1, 3), rbeta(10, 1, 3), rbeta(10, 3, 5), rbeta(10, 4, 5), rbeta(10, 6, 3))
   dpobjlistTest <- DirichletProcessHierarchicalBeta(dataTest, 1)
 
   dpobjlistTest <- ClusterComponentUpdate(dpobjlistTest)
 
+  for(i in seq_along(dpobjlistTest$indDP)){
+    expect_true(all(c(dpobjlistTest$indDP[[i]]$clusterParameters[[1]]) %in% c(dpobjlistTest$globalParameters[[1]])))
+    expect_true(all(c(dpobjlistTest$indDP[[i]]$clusterParameters[[2]]) %in% c(dpobjlistTest$globalParameters[[2]])))
+  }
+})
+
+test_that("Hierarchical Mv Normal 5 Data", {
+  require(mvtnorm)
+  dataTest <- list(rmvnorm(100, c(0,0), diag(2)), rmvnorm(100, c(1,1), diag(2)), rmvnorm(100, c(-1,-1), diag(2)), rmvnorm(100, c(2,2), diag(2)), rmvnorm(100, c(-2,-2), diag(2)))
+  dpobjlistTest <- DirichletProcessHierarchicalMvnormal2(dataTest)
+  
+  dpobjlistTest <- ClusterComponentUpdate(dpobjlistTest)
+  
   for(i in seq_along(dpobjlistTest$indDP)){
     expect_true(all(c(dpobjlistTest$indDP[[i]]$clusterParameters[[1]]) %in% c(dpobjlistTest$globalParameters[[1]])))
     expect_true(all(c(dpobjlistTest$indDP[[i]]$clusterParameters[[2]]) %in% c(dpobjlistTest$globalParameters[[2]])))
