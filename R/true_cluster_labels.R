77f1a327e690f7214732ee6101d85b5272d10112
giovanni-sig <giovanni.sighinolfi2@studio.unibo.it>
2020-03-20 17:45:20 +0100

    Added function to select the true cluster labels in higher dimensions

---
+++ R/true_cluster_labels.R
@@ -0,0 +1,24 @@
+#' Identifies the correct clusters labels, in any dimension, 
+#' when cluster parameters and global parameters are matched.
+#'
+#' @param array The array containing matching indexes.
+#' @param dpobj A hierarchical dirichletprocess object.
+#'
+#' @return The array containing the correct matching indexes
+#' @export
+
+
+true_cluster_labels <- function(array, dpObj) {
+  data <- dpObj$indDP[[1]]$data
+  
+  if (ncol(data) == 1) {
+    return(array)
+  } else {
+    
+    delta <- ncol(data)
+    k <- length(array)/delta
+    array <- array[delta*seq_len(k)]
+    array <- array/delta
+    return(array)
+  }
+}
