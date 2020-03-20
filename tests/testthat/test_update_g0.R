ff4da5005bed8132af47affd454eae0704d76dfe
giovanni-sig <giovanni.sighinolfi2@studio.unibo.it>
2020-03-20 18:13:14 +0100

    Added tests for higher dimension

--- tests/testthat/test_update_g0.R
+++ tests/testthat/test_update_g0.R
@@ -32,6 +32,37 @@ test_that("2 Data, 1 Cluster", {
 
 })
 
+test_that("2 Data, 1 Cluster, 2D", {
+  require(mvtnorm)
+  dataTest <- list(rmvnorm(100, c(0,0), diag(2)), rmvnorm(100, c(1,1), diag(2)))
+  dpobjlistTest <- DirichletProcessHierarchicalMvnormal2(dataTest)
+  
+  preCP <- list()
+  preCP[[1]] <- array(c(c(0,0)), dim=c(1,2,1))
+  preCP[[2]] <- array(c(diag(2)), dim=c(2,2,1))
+  
+  preNumCluster <- 1
+  preLabels <- rep_len(1, 100)
+  prePointsPerCluster <- 100
+  
+  dpobjlistTest$globalParameters <- preCP
+  
+  for(i in seq_along(dpobjlistTest$indDP)){
+    dpobjlistTest$indDP[[i]]$numberClusters <- preNumCluster
+    dpobjlistTest$indDP[[i]]$clusterLabels <- preLabels
+    dpobjlistTest$indDP[[i]]$pointsPerCluster <- prePointsPerCluster
+    dpobjlistTest$indDP[[i]]$clusterParameters <- preCP
+    
+  }
+  
+  dpobjlistTest <- UpdateG0(dpobjlistTest)
+  
+  for(i in seq_along(dpobjlistTest$indDP)){
+    expect_equal(dpobjlistTest$indDP[[i]]$clusterParameters[[1]][,,1], dpobjlistTest$globalParameters[[1]][,,1])
+  }
+  
+})
+
 test_that("5 Data Cluster Component then G0", {
 
   dataTest <- list(rbeta(10, 2, 3), rbeta(10, 1, 3), rbeta(10, 5, 3), rbeta(10, 6, 2), rbeta(10, 9, 4))
@@ -40,6 +71,19 @@ test_that("5 Data Cluster Component then G0", {
   dpobjlistTest <- ClusterComponentUpdate(dpobjlistTest)
   dpobjlistTest <- UpdateG0(dpobjlistTest)
 
+  for(i in seq_along(dpobjlistTest$indDP)){
+    expect_true(all(c(dpobjlistTest$indDP[[i]]$clusterParameters[[1]]) %in% c(dpobjlistTest$globalParameters[[1]])))
+  }
+})
+
+test_that("5 Data Cluster Component then G0, 2D", {
+  
+  dataTest <- list(rmvnorm(100, c(0,0), diag(2)), rmvnorm(100, c(1,1), diag(2)), rmvnorm(100, c(-1,-1), diag(2)), rmvnorm(100, c(2,2), diag(2)), rmvnorm(100, c(-2,-2), diag(2)))
+  dpobjlistTest <- DirichletProcessHierarchicalMvnormal2(dataTest)
+  
+  dpobjlistTest <- ClusterComponentUpdate(dpobjlistTest)
+  dpobjlistTest <- UpdateG0(dpobjlistTest)
+  
   for(i in seq_along(dpobjlistTest$indDP)){
     expect_true(all(c(dpobjlistTest$indDP[[i]]$clusterParameters[[1]]) %in% c(dpobjlistTest$globalParameters[[1]])))
   }
@@ -54,6 +98,20 @@ test_that("5 Data Cluster Component, Global Param then G0", {
   dpobjlistTest <- GlobalParameterUpdate(dpobjlistTest)
   dpobjlistTest <- UpdateG0(dpobjlistTest)
 
+  for(i in seq_along(dpobjlistTest$indDP)){
+    expect_true(all(c(dpobjlistTest$indDP[[i]]$clusterParameters[[1]]) %in% c(dpobjlistTest$globalParameters[[1]])))
+  }
+})
+
+test_that("5 Data Cluster Component, Global Param then G0, 2D", {
+  require(mvtnorm)
+  dataTest <- list(rmvnorm(100, c(0,0), diag(2)), rmvnorm(100, c(1,1), diag(2)), rmvnorm(100, c(-1,-1), diag(2)), rmvnorm(100, c(2,2), diag(2)), rmvnorm(100, c(-2,-2), diag(2)))
+  dpobjlistTest <- DirichletProcessHierarchicalMvnormal2(dataTest)
+  
+  dpobjlistTest <- ClusterComponentUpdate(dpobjlistTest)
+  dpobjlistTest <- GlobalParameterUpdate(dpobjlistTest)
+  dpobjlistTest <- UpdateG0(dpobjlistTest)
+  
   for(i in seq_along(dpobjlistTest$indDP)){
     expect_true(all(c(dpobjlistTest$indDP[[i]]$clusterParameters[[1]]) %in% c(dpobjlistTest$globalParameters[[1]])))
   }
